84343439083ec9629770a74997af3dfbd759fcf1
sreeniindela <sreenu.indela91@gmail.com>
2024-05-10 11:47:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,8 @@
+resource "azurerm_resource_group" "example02" {
+  name     = "example02"
+  location = "West Europe"
+}
+resource "azurerm_resource_group" "example03" {
+  name     = "example03"
+  location = "West Europe"
+}
