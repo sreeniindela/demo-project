a4d9a0af557e4ff5d0185c2b1bd22f5475c253a8
sreeniindela <sreenu.indela91@gmail.com>
2024-05-14 14:26:00 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
 provider "azurerm" {
   features {} 
   client_id       = "6c725fe4-894e-4626-99f2-5e6a87f29c07"
-  client_secret   = "SsI8Q~5F4Lq25YK2rqgo4F6c9ocxfmX-upvBnb-a" 
+  client_secret   = "2f41bf7f-1cf1-4920-a4d9-2ac7f858dac9" 
   tenant_id       = "ef6cffce-16f0-447b-bf55-9a009d89f322"
   subscription_id = "bb5c97fd-300b-4673-ab88-2c6e259f6917"
 }
